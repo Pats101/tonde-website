e3ee327cc73e91ac74f0686777b7788f41fa33c9
Patrick Tonderai Ganhiwa <ptganhiwa8@gmail.com>
2022-02-09 04:03:21 +0200

    worked on the scroll-up button and also on the popup menu block as well as the typing text animation

--- script.js
+++ script.js
@@ -4,12 +4,38 @@ $(document).ready(function(){
             $('.navbar').addClass("sticky");
         } else {
             $('.navbar').removeClass("sticky");
-        }   
+        } 
+        if(this.scroll > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // Slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
     });
 
     // Toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing animation script
+    var typed = new Typed(".typing", {
+        strings: ["a Software Developer", "always Developing", "a Pencil Artist"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // typing animation script
+    var typed = new Typed(".typing-2", {
+        strings: ["a Software Developer", "always Developing", "a Pencil Artist"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
     });
 });
